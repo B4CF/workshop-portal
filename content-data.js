ff050a35409f6183954d8ced9c163d3f5a06e06d
B4CF <outreach@bridge4change.org>
2026-02-15 01:06:49 +0530

    Add files via upload

---
+++ content-data.js
@@ -0,0 +1,516 @@
+const contentData = {
+  "videos": [
+    {
+      "id": 1,
+      "title": "Documentary_Breaking Boundaries",
+      "theme": "Earth, Ecology & Climate Change",
+      "duration": "1:14:00",
+      "link": "https://www.dailymotion.com/video/x8h1bck",
+      "quiz_name": "Breaking Boundaries - Quiz",
+      "quiz_link": "https://docs.google.com/document/d/1ej7St0dji0MLK1MeguwKnOegpaG78bme8Px7lPZmVf8/edit?usp=sharing",
+      "quiz_duration": "0:15:00"
+    },
+    {
+      "id": 2,
+      "title": "Documentary_Minimalism",
+      "theme": "Lifestyle",
+      "duration": "1:18:00",
+      "link": "https://www.youtube.com/watch?v=J8DGjUv-Vjc",
+      "quiz_name": "Minimalism - Quiz",
+      "quiz_link": "https://docs.google.com/document/d/1b2a7NVsQJsc-jG3H_J5qEra1_CMMAXzpmFfzHi6h2cI/edit?usp=sharing",
+      "quiz_duration": "0:15:00"
+    },
+    {
+      "id": 3,
+      "title": "Documentary_Seaspiracy",
+      "theme": "Earth, Ecology & Climate Change",
+      "duration": "1:30:00",
+      "link": "https://vimeo.com/532339379",
+      "quiz_name": "Seaspiracy- Quiz",
+      "quiz_link": "https://docs.google.com/document/d/17xTSyqBGsD3_rmNYcL8Hjvta0lRJl68R2UFenpWgKms/edit?usp=sharing",
+      "quiz_duration": "0:15:00"
+    },
+    {
+      "id": 4,
+      "title": "Documentary_Frozen World",
+      "theme": "Earth, Ecology & Climate Change",
+      "duration": "0:53:00",
+      "link": "https://www.youtube.com/watch?v=cTQ3Ko9ZKg8",
+      "quiz_name": "Frozen World - Quiz",
+      "quiz_link": "https://docs.google.com/document/d/178iOjn7uvDoNQfoAMlXt7CwfXI9YEd0F-Jxxp712qww/edit?usp=sharing",
+      "quiz_duration": "0:15:00"
+    },
+    {
+      "id": 5,
+      "title": "Documentary_Our Planet",
+      "theme": "Earth, Ecology & Climate Change",
+      "duration": "0:48:00",
+      "link": "https://www.youtube.com/watch?v=JkaxUblCGz0",
+      "quiz_name": "Our Planet - Quiz",
+      "quiz_link": "https://docs.google.com/document/d/1dIjRxROV7UEX2sozaG1CO4FRKvFGnTpfmCZ9a1V7UKQ/edit?usp=sharing",
+      "quiz_duration": "0:10:00"
+    },
+    {
+      "id": 6,
+      "title": "Short Video_Carbon",
+      "theme": "Climate Change",
+      "duration": "0:25:00",
+      "link": "https://www.youtube.com/watch?v=zMVpwc1nO2k",
+      "quiz_name": "Carbon - Quiz",
+      "quiz_link": "https://docs.google.com/document/d/1plINdJNb91m-4_QAsdSmQ3FUzwkElTh_n4p7KZL_GSM/edit?usp=sharing",
+      "quiz_duration": "0:10:00"
+    },
+    {
+      "id": 7,
+      "title": "Short Video_A Solution to the Pollution of Cooling India",
+      "theme": "Climate Change & Energy Conservation",
+      "duration": "0:17:00",
+      "link": "https://www.youtube.com/watch?v=kr1Jaz_lwp4",
+      "quiz_name": "A Solution to the Pollution of Cooling India - Quiz",
+      "quiz_link": "https://docs.google.com/document/d/1VmQfxCNF0KDu87GSpfSJkVK4ay6cY6sCrWUn9402ZWA/edit?usp=sharing",
+      "quiz_duration": "0:10:00"
+    },
+    {
+      "id": 8,
+      "title": "TAGR Videos_Flight",
+      "theme": "Lifestyle - Transport",
+      "duration": "0:02:58",
+      "link": "https://youtu.be/FtJEyEIMFtA?si=U1DuCBFV0eE8Mmhr",
+      "quiz_name": "YIAM Quiz",
+      "quiz_link": "https://docs.google.com/document/d/1a8ghkOG4Jn7RBC3H6X5FL9a7f6nrvd1wO4_TUKqGGpQ/edit?usp=drive_link",
+      "quiz_duration": "0:30:00"
+    },
+    {
+      "id": 9,
+      "title": "TAGR Videos_Tea",
+      "theme": "Lifestyle - Food",
+      "duration": "0:03:39",
+      "link": "https://youtu.be/yv19alrt_2M?si=GQ6-9cv7A3wXxfPe",
+      "quiz_name": null,
+      "quiz_link": null,
+      "quiz_duration": null
+    },
+    {
+      "id": 10,
+      "title": "TAGR Videos_Jeans",
+      "theme": "Lifestyle - Clothing",
+      "duration": "0:03:31",
+      "link": "https://youtu.be/xiFmvhQ8SXw?si=bl7TAAjn6B-VjvGb",
+      "quiz_name": null,
+      "quiz_link": null,
+      "quiz_duration": null
+    },
+    {
+      "id": 11,
+      "title": "TAGR Videos_Carpool",
+      "theme": "Lifestyle - Transport",
+      "duration": "0:03:17",
+      "link": "https://youtu.be/sZmmw78BQVw?si=M2j4F9Xl1MVmnEx-",
+      "quiz_name": null,
+      "quiz_link": null,
+      "quiz_duration": null
+    },
+    {
+      "id": 12,
+      "title": "TAGR Videos_Cheese",
+      "theme": "Lifestyle - Food",
+      "duration": "0:03:17",
+      "link": "https://youtu.be/RmpaGG4XMac?si=xFurDK0GKyK0a2ys",
+      "quiz_name": null,
+      "quiz_link": null,
+      "quiz_duration": null
+    },
+    {
+      "id": 13,
+      "title": "TAGR Videos_Public Transport",
+      "theme": "Lifestyle - Transport",
+      "duration": "00:02:44",
+      "link": "https://youtu.be/SrfU8nns-_M?si=DdLK2uGEyfBnbQpc",
+      "quiz_name": null,
+      "quiz_link": null,
+      "quiz_duration": null
+    },
+    {
+      "id": 14,
+      "title": "TAGR Videos_Cloth Bag",
+      "theme": "Lifestyle - Waste",
+      "duration": "00:02:36",
+      "link": "https://youtu.be/eYA9O25uJao?si=g_J7KjWzkrYxEOEb",
+      "quiz_name": null,
+      "quiz_link": null,
+      "quiz_duration": null
+    },
+    {
+      "id": 15,
+      "title": "TAGR Videos_Chicken",
+      "theme": "Lifestyle - Food",
+      "duration": "00:03:25",
+      "link": "https://youtu.be/VT34IwotbRw?si=_kKOBa1Zc2hMZ_E9",
+      "quiz_name": null,
+      "quiz_link": null,
+      "quiz_duration": null
+    },
+    {
+      "id": 16,
+      "title": "TAGR Videos_Chocolate",
+      "theme": "Lifestyle - Food",
+      "duration": "00:03:02",
+      "link": "https://youtu.be/QmgfFyrCoos?si=RgmR1VgUulOC1EZP",
+      "quiz_name": null,
+      "quiz_link": null,
+      "quiz_duration": null
+    },
+    {
+      "id": 17,
+      "title": "TAGR Videos_Food Waste",
+      "theme": "Lifestyle - Waste",
+      "duration": "00:03:17",
+      "link": "https://youtu.be/EKGnYcOuNVY?si=5p-NtZvz9ihmYU04",
+      "quiz_name": null,
+      "quiz_link": null,
+      "quiz_duration": null
+    },
+    {
+      "id": 18,
+      "title": "TAGR Videos_Digital Detox",
+      "theme": "Lifestyle - Energy",
+      "duration": "00:03:01",
+      "link": "https://youtu.be/gXKf3g1ZUds?si=yZhhUeiqNNlUUEOM",
+      "quiz_name": null,
+      "quiz_link": null,
+      "quiz_duration": null
+    },
+    {
+      "id": 19,
+      "title": "TAGR Videos_Lift",
+      "theme": "Lifestyle - Transport",
+      "duration": "00:03:06",
+      "link": "https://youtu.be/ZYmQlioMt2s?si=4OJnRB6vVM6f4WfA",
+      "quiz_name": null,
+      "quiz_link": null,
+      "quiz_duration": null
+    },
+    {
+      "id": 20,
+      "title": "TAGR Videos_Tissue Paper",
+      "theme": "Lifestyle - Waste",
+      "duration": "00:02:56",
+      "link": "https://youtu.be/xS8koYJxo6k?si=5fpewLmLmvtE2_3L",
+      "quiz_name": null,
+      "quiz_link": null,
+      "quiz_duration": null
+    },
+    {
+      "id": 21,
+      "title": "TAGR Videos_Waste Segregation",
+      "theme": "Lifestyle - Waste",
+      "duration": "00:04:20",
+      "link": "https://youtu.be/xToynONvYaw?si=1ILSq5_tmvKmEkyn",
+      "quiz_name": null,
+      "quiz_link": null,
+      "quiz_duration": null
+    },
+    {
+      "id": 22,
+      "title": "TAGR Videos_Wi-Fi Router",
+      "theme": "Lifestyle - Energy",
+      "duration": "00:03:12",
+      "link": "https://youtu.be/0UkYdDkvKnM?si=B__A2ih8u-ymiTtP",
+      "quiz_name": null,
+      "quiz_link": null,
+      "quiz_duration": null
+    },
+    {
+      "id": 23,
+      "title": "TAGR Videos_Light Bulb",
+      "theme": "Lifestyle - Energy",
+      "duration": "00:02:14",
+      "link": "https://youtu.be/bo1yA0IcOHc?si=XT9Z5ZZjRElYNVeA",
+      "quiz_name": null,
+      "quiz_link": null,
+      "quiz_duration": null
+    },
+    {
+      "id": 24,
+      "title": "Mindful Living Tips_Reading Everyday",
+      "theme": "Lifestyle - Personal Well-being",
+      "duration": "00:01:04",
+      "link": "https://youtube.com/shorts/ovitwAmBnl4?si=-quF9mx5sG_1-XNQ",
+      "quiz_name": null,
+      "quiz_link": null,
+      "quiz_duration": null
+    },
+    {
+      "id": 25,
+      "title": "Mindful Living Tips_Importance of Sleep",
+      "theme": "Lifestyle - Personal Well-being",
+      "duration": "00:01:01",
+      "link": "https://youtube.com/shorts/TXAp-BCNG8Q?si=uAKP3kHPkc_pGIO8",
+      "quiz_name": null,
+      "quiz_link": null,
+      "quiz_duration": null
+    },
+    {
+      "id": 26,
+      "title": "Mindful Living Tips_Public Transport and Carpooling",
+      "theme": "Lifestyle - Transport",
+      "duration": "00:01:00",
+      "link": "https://youtu.be/ghJdErxxQeA?si=H28sU54Zz1_lebjg",
+      "quiz_name": null,
+      "quiz_link": null,
+      "quiz_duration": null
+    },
+    {
+      "id": 27,
+      "title": "Mindful Living Tips_Wake Up Early",
+      "theme": "Lifestyle - Personal Well-being",
+      "duration": "00:01:02",
+      "link": "https://youtu.be/G0g1MGqk5JM?si=kL2M1_RbLFQoYs8c",
+      "quiz_name": null,
+      "quiz_link": null,
+      "quiz_duration": null
+    },
+    {
+      "id": 28,
+      "title": "Mindful Living Tips_Early & Light Dinner",
+      "theme": "Lifestyle - Personal Well-being",
+      "duration": "00:01:29",
+      "link": "https://youtu.be/ZC-WFuVUGlE?si=vPy3ZF73cvcGN88Z",
+      "quiz_name": null,
+      "quiz_link": null,
+      "quiz_duration": null
+    },
+    {
+      "id": 29,
+      "title": "Mindful Living Tips_Drinking Water",
+      "theme": "Lifestyle - Personal Well-being",
+      "duration": "00:00:52",
+      "link": "https://youtu.be/PjXIHhMzRHc?si=YnZXafA2ooiH6YzC",
+      "quiz_name": null,
+      "quiz_link": null,
+      "quiz_duration": null
+    },
+    {
+      "id": 30,
+      "title": "Mindful Living Tips_Gratitude Journaling",
+      "theme": "Lifestyle - Personal Well-being",
+      "duration": "00:01:21",
+      "link": "https://youtu.be/Lux4RUV-iNw?si=wz5Fo-pf30JHsgJF",
+      "quiz_name": null,
+      "quiz_link": null,
+      "quiz_duration": null
+    },
+    {
+      "id": 31,
+      "title": "Mindful Living Tips_Mobile on Silent/Airplane Mode",
+      "theme": "Lifestyle - Energy",
+      "duration": "00:01:11",
+      "link": "https://youtu.be/CYhJhvjzlZI?si=B7DP4H_Lx_IoWtPK",
+      "quiz_name": null,
+      "quiz_link": null,
+      "quiz_duration": null
+    },
+    {
+      "id": 32,
+      "title": "Mindful Living Tips_Exercise",
+      "theme": "Lifestyle - Personal Well-being",
+      "duration": "00:01:01",
+      "link": "https://youtu.be/tQ6JjHgykKA?si=LSEPHOiTcIr4MD28",
+      "quiz_name": null,
+      "quiz_link": null,
+      "quiz_duration": null
+    },
+    {
+      "id": 33,
+      "title": "Mindful Living Tips_Gardening",
+      "theme": "Lifestyle - Personal Well-being",
+      "duration": "00:01:01",
+      "link": "https://youtu.be/AI-bbVK8ev0?si=qv-2zTPxzdgEpXQ1",
+      "quiz_name": null,
+      "quiz_link": null,
+      "quiz_duration": null
+    },
+    {
+      "id": 34,
+      "title": "Mindful Living Tips_Morning Yoga",
+      "theme": "Lifestyle - Personal Well-being",
+      "duration": "00:01:01",
+      "link": "https://youtu.be/D1rMJu7aPs4?si=azexkDn0dciSIHlC",
+      "quiz_name": null,
+      "quiz_link": null,
+      "quiz_duration": null
+    },
+    {
+      "id": 35,
+      "title": "Mindful Living Tips_Cutting a Milk Packet",
+      "theme": "Lifestyle - Waste",
+      "duration": "00:00:32",
+      "link": "https://youtu.be/pwlauTqzy4I?si=8cZYVp_WshoA-Aw-",
+      "quiz_name": null,
+      "quiz_link": null,
+      "quiz_duration": null
+    },
+    {
+      "id": 36,
+      "title": "Mindful Living Tips_Morning Exercise",
+      "theme": "Lifestyle - Personal Well-being",
+      "duration": "00:00:57",
+      "link": "https://youtu.be/k3m0c2Cj_lQ?si=Fujp4o5V9QxtZ-I0",
+      "quiz_name": null,
+      "quiz_link": null,
+      "quiz_duration": null
+    },
+    {
+      "id": 37,
+      "title": "Mindful Living Tips_Cutting A Plastic Packet",
+      "theme": "Lifestyle - Waste",
+      "duration": "00:00:56",
+      "link": "https://youtu.be/t6hG40Ahqfs?si=XImadQvgUTPmLewb",
+      "quiz_name": null,
+      "quiz_link": null,
+      "quiz_duration": null
+    },
+    {
+      "id": 38,
+      "title": "Social Issue_Combating AQI - The silent killer",
+      "theme": "Climate Change",
+      "duration": "00:04:15",
+      "link": "https://youtu.be/PIiCD44cKMc?si=frkjuL-pMfalWm4V",
+      "quiz_name": "Combating AQI - The silent killer - Quiz",
+      "quiz_link": "https://docs.google.com/document/d/1nH2AFSln8orLZcBLSqAGDICIM6KkazGQJ_F6TyniYSk/edit?usp=sharing",
+      "quiz_duration": "0:15:00"
+    },
+    {
+      "id": 39,
+      "title": "Tiny Shifts To Aim For mitigation of Climate Change",
+      "theme": "Lifestyle",
+      "duration": "00:04:23",
+      "link": "https://youtu.be/oEpHr2poU4Q?si=Cy1zQNQjYVjT8eLm",
+      "quiz_name": null,
+      "quiz_link": null,
+      "quiz_duration": null
+    },
+    {
+      "id": 40,
+      "title": "Impact of Tiny Shifts on Climate Change",
+      "theme": "Lifestyle - Impact",
+      "duration": "00:03:39",
+      "link": "https://youtu.be/6WKQqgE2QRs?si=e6ypTuSpJbz8i4WN",
+      "quiz_name": null,
+      "quiz_link": null,
+      "quiz_duration": null
+    },
+    {
+      "id": 41,
+      "title": "Carbon Emission Calculation",
+      "theme": "Calculation",
+      "duration": "00:01:28",
+      "link": "https://youtu.be/pkHO5Bza6nU?si=9uo9Id0aJB2tq0MV",
+      "quiz_name": null,
+      "quiz_link": null,
+      "quiz_duration": null
+    },
+    {
+      "id": 42,
+      "title": "Waste Management model of Indore, India's Cleanest City",
+      "theme": "Waste Management",
+      "duration": "00:03:40",
+      "link": "https://www.youtube.com/watch?v=Whz4WEdTsCc&t=31s",
+      "quiz_name": "Waste Management model of Indore, India's Cleanest City - Quiz",
+      "quiz_link": "https://docs.google.com/document/d/1iSXMf5CIqhzq9Tl_oQ5kppVBsL6uv65gwRdxQpWHX20/edit?usp=sharing",
+      "quiz_duration": "0:15:00"
+    },
+    {
+      "id": 43,
+      "title": "Perspective Building_The Story of Stuff",
+      "theme": "Lifestyle",
+      "duration": "00:21:15",
+      "link": "https://www.youtube.com/watch?v=9GorqroigqM",
+      "quiz_name": "The Story of Stuff - Quiz",
+      "quiz_link": "https://docs.google.com/document/d/1rZ19-e4NvmEQLVpY_dGD_PkEqPB5Vb5osDydtFQ9hoo/edit?usp=sharing",
+      "quiz_duration": "0:20:00"
+    },
+    {
+      "id": 44,
+      "title": "Simon Sinek (Golden Circles)",
+      "theme": "Lifestyle",
+      "duration": "00:17:41",
+      "link": "https://www.ted.com/talks/simon_sinek_how_great_leaders_inspire_action",
+      "quiz_name": null,
+      "quiz_link": null,
+      "quiz_duration": null
+    }
+  ],
+  "articles": [
+    {
+      "id": 1,
+      "title": "The 5 Most Common Regrets",
+      "theme": "Purpose",
+      "duration": "0:20:00",
+      "link": "https://www.yourtango.com/self/most-common-regrets-people-have-right-before-they-die",
+      "quiz_name": null,
+      "quiz_link": null,
+      "quiz_duration": null
+    },
+    {
+      "id": 2,
+      "title": "15 ways to Improve Writing",
+      "theme": "Communication Skills",
+      "duration": "0:30:00",
+      "link": "https://drive.google.com/file/d/1bdEzA2mR06z0JsKkvMamW7-fJoJEYQQB/view?usp=drive_link",
+      "quiz_name": null,
+      "quiz_link": null,
+      "quiz_duration": null
+    },
+    {
+      "id": 3,
+      "title": "Witnessing the Consumption Pattern Exercise",
+      "theme": "Reflection & Empathy",
+      "duration": "0:30:00",
+      "link": "https://drive.google.com/file/d/1yFkYjZ2nJhDXvIkL5oA9zxMQIICASb3z/view?usp=drive_link",
+      "quiz_name": null,
+      "quiz_link": null,
+      "quiz_duration": null
+    },
+    {
+      "id": 4,
+      "title": "Bridge4Change Case Study",
+      "theme": "Social Enterprise",
+      "duration": "0:30:00",
+      "link": "https://drive.google.com/file/d/1HG4UAsX-Gg_LbxaONK-7wUYrewiKGbF-/view?usp=drive_link",
+      "quiz_name": "Present in Case Study Itself",
+      "quiz_link": "Present in Case Study Itself",
+      "quiz_duration": "0:45:00"
+    },
+    {
+      "id": 5,
+      "title": "Low Carbon Lifestyle (Based on Videos Sheet)",
+      "theme": "Lifestyle",
+      "duration": "",
+      "link": "https://drive.google.com/file/d/1fTJAyZj-xflblSLSeQswfDf8cQl1OmEb/view?usp=drive_link",
+      "quiz_name": "Low Carbon Lifestyle Tool",
+      "quiz_link": "https://docs.google.com/spreadsheets/d/1q74uE8ia_B7PRroY0aCgXXvAcv0JDCABCmcoOEo4GBE/edit?usp=sharing",
+      "quiz_duration": "0:45:00"
+    },
+    {
+      "id": 1,
+      "title": "IGBC Green Residential Societies Rating System",
+      "theme": "Frameworks",
+      "duration": "1:00:00",
+      "link": "https://drive.google.com/file/d/1KA9qakIWmIXcJu7fTaSGFQl8uG69JxHr/view?usp=drive_link",
+      "quiz_name": "Green Campus Project ",
+      "quiz_link": "https://docs.google.com/document/d/1dy_W5c2ffU_y2bFDhel1zMc2N8JpQVjUz-gCdFAaToE/edit?usp=sharing",
+      "quiz_duration": "1:00:00"
+    },
+    {
+      "id": 2,
+      "title": "Green University Campuses",
+      "theme": "Sustainability Examples",
+      "duration": "0:10:00",
+      "link": "https://thebetterindia.com/445516/sustainability-indian-universities-green-campuses/",
+      "quiz_name": "-",
+      "quiz_link": "-",
+      "quiz_duration": "-"
+    }
+  ]
+};
